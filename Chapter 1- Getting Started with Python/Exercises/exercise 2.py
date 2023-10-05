5a69781f7fce9bdf981ed210c3a725ba16d4e0ca
kairi <kairi.cabais@gmail.com>
2023-10-05 10:55:26 +0400

    exercise 2.py

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise 2.py
@@ -0,0 +1,6 @@
+import sys
+print ("""3.5.2 (default, Sep 10 2016, 08:21:44""")
+"""[GCC 5.4.0 20160609]"")"end=
+print (sys.version)
+print (major=3, minor=5, micro=2, releaselevel='final', serial=0)
+print (sys.version_info)"""
