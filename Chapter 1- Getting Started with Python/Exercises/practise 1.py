1d9fe1352dacbbb7a703899da14362a89c76add2
kairi <kairi.cabais@gmail.com>
2023-10-05 10:48:48 +0400

    practise 1.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practise 1.py
@@ -0,0 +1,28 @@
+print ("Name", "=Kairi", 12345)
+print ("Father's Name","Legendary Sauce",6789)
+print ("abc","xyz",23849)
+
+
+#how to take input from user
+
+age = input ("your age")
+print (age)
+name1 = input ("Enter University Name")
+print (name1)
+
+#order of operations
+print ((2*3/8+(23+5)))
+
+#type cast float to int
+ans = ((2*3/8+(23+5)))
+print (int (ans))
+
+
+
+
+
+#to write formulas into more than 1 line, use a slash
+
+
+#string concatenation 
+
