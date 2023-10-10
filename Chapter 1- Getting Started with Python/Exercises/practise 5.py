d21d5d120eb0196d560a7e88fb9b66db11c8165f
kairi <kairi.cabais@gmail.com>
2023-10-10 22:21:37 +0400

    practise 5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practise 5.py
@@ -0,0 +1,13 @@
+#String Concatenation
+var1 = "Name"
+var2 = "Age"
+var3 = "School"
+var4 = "Country"
+
+# format function is used here to 
+# combine the string
+
+# store the result in another variable 
+var5 = "{} {} {} {}".format(var1, var2, var3, var4)
+ 
+print(var5)
