fce570ea978f13fd50ad8d31560f095c3ec65ba7
kairi <kairi.cabais@gmail.com>
2023-10-10 20:53:37 +0400

    exercise 3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/demo1.py
@@ -0,0 +1,44 @@
+
+# Taking input from user
+name1 = input ("your favorite sauce")
+
+
+name2 = input ("your karate status")
+
+
+name3 = input ("your talent")
+
+
+
+
+
+name4 = ("your favorite number")
+print (name4)
+
+name5 = ("your favorite type of cooked eggs")
+print (name5)
+
+name6 = ("your favorite US state")
+print (name6)
+
+name7 = ("your phone brand")
+print ("name7")
+
+name8 = ("your favorite car brand")
+print (name8)
+
+name9 = ("your favorite fruit drink")
+print (name9)
+
+name10 = ("your last name")
+print (name10)
+
+
+
+
+# prints this is to make it easietr to read the users input answers at the end !
+print (name1)
+print (name2)
+print (name3)
+
+
---
+++ Chapter 1- Getting Started with Python/Exercises/exercise 3.py
@@ -0,0 +1,5 @@
+# Display the current date and time
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time : 2023 , 1, 6, 15, 8, 24, 78915 ")
+print (now.strftime("%Y-%m-%d %H:%M:%S")) 
