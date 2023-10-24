47693de86c74ce00eadb9ae97c5e8b9bd25f0c73
kairi <kairi.cabais@gmail.com>
2023-10-25 00:28:38 +0400

    chapter3-exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/chapter3-exercise4.py
@@ -0,0 +1,15 @@
+# Exercise 4: Guest List 
+
+#If you could invite anyone, living or deceased, to dinner, who would you invite?
+##Make a list that includes at least three people you’d like to invite to dinner. 
+guests = [' Mr. Marco Bucci', 'Ms. Amala Dlamini', 'Mr. Damini Ogulu']
+
+#Then use your list to print a message to each person, inviting them to dinner.
+name = guests[0].title()
+print("Greetings," + name + ", I would be pleased to have you tonight for dinner.")
+
+name = guests[1].title()
+print("Warm Welcomes, " + name + ", It would be an honor to have you join me for dinner.")
+
+name = guests[2].title()
+print("Sweet Greetings, " + name + ", you are welcome to join me tonight, for dinner.")
