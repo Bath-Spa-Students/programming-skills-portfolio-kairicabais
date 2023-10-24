5aec308fadf41426189b6e462fa0b5ebe1fec3f4
kairi <kairi.cabais@gmail.com>
2023-10-24 23:11:45 +0400

    chapter3-exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/chapter3-exercise2.py
@@ -0,0 +1,16 @@
+# Exercise 2: Greetings
+
+# Start with the list in Exercise 1
+
+# change instead of just printing each person’s name, print a message to them. 
+##The text of each message should be the same, but each message should be personalized with the person’s name.
+names = ['Mark', 'Duaa', 'Pristine']
+
+msg = "Mellow " + names[0].title() + "."
+print(msg)
+
+msg = "Daring " + names[1].title() + "."
+print(msg)
+
+msg = "Precious "   + names[2].title() + "."
+print(msg)
