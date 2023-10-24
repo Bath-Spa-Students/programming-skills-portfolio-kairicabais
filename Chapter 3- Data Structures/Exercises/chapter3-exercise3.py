046ef16e6cae92c87aa3fcc74b0398295bc499cd
kairi <kairi.cabais@gmail.com>
2023-10-24 23:12:11 +0400

    chapter3-exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/chapter3-exercise3.py
@@ -0,0 +1,9 @@
+# Exercise 3: Your Own List 
+
+# Make a list that stores several examples of your favorite mode of transportation. 
+## i will make a list called "items" for my modes of transportation transportation.
+items=["car","horse carriage","ferry"]
+for i in items:
+
+#print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+ print("I would love to ride a",i)
