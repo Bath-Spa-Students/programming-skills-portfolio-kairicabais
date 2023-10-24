c52a922a4c138fef9035bdd25dd9258d6cf1f2c0
kairi <kairi.cabais@gmail.com>
2023-10-24 21:33:29 +0400

    chapter2 - exercise 1.py

---
+++ Chapter 2- Variables & Comments/Exercises/chapter2- exercise1.py
@@ -0,0 +1,7 @@
+# Chapter 2, Exercise 1: Variables: Assign a message to a variable, and print that message.
+msg= "I want a new phone"
+print (msg)
+
+#Then change the value of the variable to a new message, and print the new message.
+msg= "Nevermind. I don't want a new phone"
+print (msg)
