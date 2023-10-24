164f46b7366ece4dbfd18f0f95011c1b8496ce00
kairi <kairi.cabais@gmail.com>
2023-10-24 21:48:53 +0400

    chapter2- exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/chapter2- exercise2.py
@@ -0,0 +1,5 @@
+# chapter 2, Exercise 2: Variables 
+
+#Find a quote from a famous person you admire. Print the quote and the name of its author. 
+print('Oscar Wilde once said, "Education is an admirable thing, but it is well to remember from time to time')
+print('that nothing that is worth knowing can be taught."')
