a511fe841298b338540a940cab744f3dcea6a141
kairi <kairi.cabais@gmail.com>
2023-10-28 17:18:16 +0400

    exercise 3.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 3.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 3.py
@@ -1,4 +1,7 @@
-# Display the current date and time
+# Chapter 1, Exercise 3: Print date and Time
+
+
+#Display the current date and time
 import datetime
 now = datetime.datetime.now()
 print ("Current date and time : 2023 , 1, 6, 15, 8, 24, 78915 ")
