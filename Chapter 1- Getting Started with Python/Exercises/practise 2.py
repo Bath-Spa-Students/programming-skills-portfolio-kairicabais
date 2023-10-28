962add527e64260e91ed46ef458e9c9dfe240bac
kairi <kairi.cabais@gmail.com>
2023-10-28 17:29:40 +0400

    practise 2.py

--- Chapter 1- Getting Started with Python/Exercises/practise 2.py
+++ Chapter 1- Getting Started with Python/Exercises/practise 2.py
@@ -1,2 +1,13 @@
-#Print "hi" 50 times
+# Chapter 1, practise 2 : Print strings to a certain amount. 
+
+# Print "hi" 50 times
 print("hi"*50)
+
+# Method 1: Use print() and multiplication operator
+saying = 'Live. Laugh. Love!\t'
+print(saying * 3)
+
+# Method 2: Use a For Loop and range()
+for i in range(5):
+    print('I love my dog so much I cant stop saying it')
+    
