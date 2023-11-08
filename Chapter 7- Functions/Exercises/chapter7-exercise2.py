4b2620ad63fb8bd6dfeafc5b006ebbf01ea2eb5d
kairi <kairi.cabais@gmail.com>
2023-11-08 17:20:44 +0400

    chapter7-exercise2.py

---
+++ Chapter 7- Functions/Exercises/chapter7-exercise2.py
@@ -0,0 +1,13 @@
+# Chapter 7, Exercise 2: Favorite Book
+
+
+
+# Write a function called favorite_book() that accepts one parameter, title.
+def favorite_book(title):
+    """Display a message about someone's favorite book."""
+
+# The function should print a message, such as One of my favorite books is Alice in Wonderland. 
+    print(title + " is one of my favorite books.")
+
+# Call the function, making sure to include a book title as an argument in the function call.
+favorite_book('The bridge of beyond by Simone Schwarz-Bart')
