ae3f4396acb8f0662970ee785d08573a9ae06cc6
kairi <kairi.cabais@gmail.com>
2023-11-08 17:18:56 +0400

    exercise 4.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 4.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 4.py
@@ -6,3 +6,27 @@ b = " are"
 c = " you?"
 d= a + b + c
 print(d)
+
+
+x = 10
+y = 20
+z = 30
+a = x + y + z
+print(a)
+
+
+print("Enter Marks Obtained in 5 Subjects: ")
+mOne = int(input())
+mTwo = int(input())
+mThree = int(input())
+mFour = int(input())
+mFive = int(input())
+
+sum = mOne+mTwo+mThree+mFour+mFive
+avg = sum/5
+perc = (sum/500)*100
+
+print(end="Average Mark = ")
+print(avg)
+print(end="Percentage Mark = ")
+print(perc)
