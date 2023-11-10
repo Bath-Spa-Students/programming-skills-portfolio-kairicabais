ad0658189380f6dfc4ea3804d0734c83e6fb2cc4
kairi <kairi.cabais@gmail.com>
2023-11-10 15:07:47 +0400

    chapter3-exercise6.py

--- Chapter 3- Data Structures/Exercises/chapter3-exercise6.py
+++ Chapter 3- Data Structures/Exercises/chapter3-exercise6.py
@@ -1,6 +1,4 @@
 # Exercise 6: Shrinking Guest List
-
-
 #My guests from previous list that can come.
 guests= ['Mr. Marco Bucci', 'Mr. Mark Merchant', 'Mr. Damini Ogulu']
 
@@ -10,8 +8,6 @@ print("\nSorry, we can only invite two people to dinner.")
 name = guests.pop()
 print("Sorry, " + name.title() + " there's no room at the table.")
 
-
-
 # •Print a message to each of the two people still on your list, letting them know they’re still invited.
 name = guests[0].title()
 print(name + ", please come to dinner.")
@@ -19,7 +15,7 @@ print(name + ", please come to dinner.")
 name = guests[1].title()
 print(name + ", please come to dinner.")
 
-# Empty out the list.
+# •Use del to remove the last two names from your list, so you have an empty list.
 del(guests[0])
 del(guests[0])
 
