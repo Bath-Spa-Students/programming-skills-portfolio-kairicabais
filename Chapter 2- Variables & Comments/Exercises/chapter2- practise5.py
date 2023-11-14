d5fdb0ffbde6eb44105a4a66a0b9607c4740adb8
kairi <kairi.cabais@gmail.com>
2023-11-14 14:04:02 +0400

    chapter2- practise5.py

---
+++ Chapter 2- Variables & Comments/Exercises/chapter2- practise5.py
@@ -0,0 +1,15 @@
+# Chapter 2, practise 5: fruit basket
+
+# Write a python program that stores fruit names in a list named fruits.
+# Unpack the list into three variables x, y and z and then print the values of each variable.
+
+# Create a list of strings
+fruits = ["dragon fruit", "mandarin", "persimmon"]
+
+# Unpack the list into variables
+x, y, z = fruits
+
+# Print the values of each variable
+print("Value of x:", x)
+print("Value of y:", y)
+print("Value of z:", z)
