c0962e71a24e4bf6eb4e03cc1d431aea8d3d0fc8
kairi <kairi.cabais@gmail.com>
2023-11-14 13:59:56 +0400

    chapter2- exercise4.py

--- Chapter 2- Variables & Comments/Exercises/chapter2- exercise 4.py
+++ Chapter 2- Variables & Comments/Exercises/chapter2- exercise 4.py
@@ -1,8 +1,8 @@
 # Chapter 2, Exercise 4: Favorite Number
-#Use a variable to represent your favorite number. 
+# Use a variable to represent your favorite number. 
 fav_num = 19
 
-#Then,using that variable, create a message that reveals your favorite number.
+# Then,using that variable, create a message that reveals your favorite number.
 msg = "My favorite number is " + str(fav_num) + "."
 # i added + to connect the message contents
 
