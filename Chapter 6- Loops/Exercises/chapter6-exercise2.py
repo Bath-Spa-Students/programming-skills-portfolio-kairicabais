238bdc467538c5f452e01b3ba8b8e0e35c6c2882
kairi <kairi.cabais@gmail.com>
2023-11-14 14:10:44 +0400

    chapter2-exercise2,py

--- Chapter 6- Loops/Exercises/chapter6-exercise2.py
+++ Chapter 6- Loops/Exercises/chapter6-exercise2.py
@@ -2,58 +2,24 @@
 
 # A movie theater charges different ticket prices depending on a person’s age.
 #Write a loop in which you ask users their age, and then tell them the cost of their movie ticket
-Movie_tickets = "How old are you?"
-Movie_tickets += "\nEnter 'quit' when you are finished. "
+user_age = "How old are you?"
+user_age += "\nEnter 'quit' when you are finished. "
 
+# to end the loop i want user to type "quit"
 while True:
-    age = input(Movie_tickets)
+    age = input(user_age)
     if age == 'quit':
         break
     age = int(age)
 # If a person is under the age of 3, the ticket is free; 
-if age < 3:
-        print("You get in free!")
-
-# if they are between 3 and 12, the ticket is $10;
-elif age < 13:     
-        print("Your ticket is $10.")
-# and if they are over age 12, the ticket is $15. 
-
-else:
-        print("Your ticket is $15.")
-
-
-bruhh = "How old are you?"
-bruhh += "\nEnter 'quit' when you are finished. "
-
-
-
-while True:
-    age = input(bruhh)
-    if age == 'quit':
-        break
-    age = int(age)
-
     if age < 3:
         print("  You get in free!")
+ # if they are between 3 and 12, the ticket is $10;
     elif age < 13:
         print("  Your ticket is $10.")
+ # and if they are over age 12, the ticket is $15.
     else:
         print("  Your ticket is $15.")
 
 
-
-
-
-    age = input("How old are you? ")
-    
-    if age == "quit":
-        break
-    age = int(age)
-    if age < 3:
-        print("Your ticket is free")
-    elif age < 12:
-        print("Your ticket is $10") 
-    else:
-        print("Your ticket is $15")
 
