41ccaf06af59a072d0179f98aa53604e24716f42
kairi <kairi.cabais@gmail.com>
2023-11-14 15:05:46 +0400

    chapter3-practise2.py

---
+++ Chapter 3- Data Structures/Exercises/chapter3-practise2.py
@@ -0,0 +1,5 @@
+# Chapter 3, practise 2 : lists
+
+# Write a statement that creates a list with the following strings: 'Einstein', Newton' , 'Copernicus', and 'Kepler'
+
+Names = ["Einstein", "Newton", "Copernicus", "Kepler"]
