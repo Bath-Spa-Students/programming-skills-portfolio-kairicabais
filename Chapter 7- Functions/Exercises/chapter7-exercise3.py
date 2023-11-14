13e4a37683ebc8bf555cd7210a0c3614f78e3377
kairi <kairi.cabais@gmail.com>
2023-11-14 14:01:09 +0400

    chapter7-exercise3.py

---
+++ Chapter 7- Functions/Exercises/chapter7-exercise3.py
@@ -0,0 +1,14 @@
+# Chapter 7, Exercise 3: T-Shirt
+
+# Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+# The function should print a sentence summarizing the size of the shirt and the message printed on it.
+# Call the function once using positional arguments to make a shirt. 
+# Call the function a second time using keyword arguments.
+
+def make_shirt(size, message):
+    """Summarize the shirt that's going to be made."""
+    print("\nI'm going to make a " + size + " t-shirt.")
+    print('It will say, "' + message + '"')
+
+make_shirt('large', 'Look at my big shirt!')
+make_shirt(message="It's my birthday!!!", size='medium')
