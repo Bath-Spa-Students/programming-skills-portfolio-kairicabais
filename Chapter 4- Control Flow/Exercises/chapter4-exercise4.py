8f52a4cc13b929e3990801871e315b26997f5fdc
kairi <kairi.cabais@gmail.com>
2023-11-14 14:11:08 +0400

    chapter4-exercise4.py

--- Chapter 4- Control Flow/Exercises/chapter4-exercise4.py
+++ Chapter 4- Control Flow/Exercises/chapter4-exercise4.py
@@ -1,11 +1,9 @@
 # Exercise 4: Stages of Life
 
-
 # Write an if-elif-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
 
 ### The first step to any code, is to set a variable. For this variable, we'll base it on age. I added a user input.
 age = float(input("Enter any human age: "))
-
 
 # •If the person is less than 2 years old, print a message that the person is a baby.
 # •If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
@@ -13,7 +11,6 @@ age = float(input("Enter any human age: "))
 # •If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
 # •If the person is at least 20 years old but less than 65, print a message that the person is an adult.
 # •If the person is age 65 or older, print a message that the person is an elder.
-
 if age < 2:
     print("You're a baby!")
 elif age < 4:
