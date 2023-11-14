d1c43f021e5ed2662b3bc01efff303cb345779b0
kairi <kairi.cabais@gmail.com>
2023-11-14 14:55:26 +0400

    chapter4-exercise3.py

--- Chapter 4- Control Flow/Exercises/chapter4-exercise3.py
+++ Chapter 4- Control Flow/Exercises/chapter4-exercise3.py
@@ -1,6 +1,5 @@
 # Exercise 3: Alien Colors #3 
 
-
 ### Turn your if-else chain from Exercise 5-4 into an if-elifelse chain.
 #  Write three versions of this program, making sure each message is printed for the appropriate color alien. In this, I decided not to include a user input.
 
